c602cbd1efaab3f66f28c3453210aa2a4e51bb04
Andrii <andrejzurba749@gmail.com>
2022-04-18 17:27:30 +0300

    Add stars work !

--- assets/main/Slider/DescribeBlock/StarsContainer/index.js
+++ assets/main/Slider/DescribeBlock/StarsContainer/index.js
@@ -6,7 +6,63 @@ class StarsContainer{
 
         for (let i = 0; i < 5; i++) {
             const element = document.createElement('img');
+            element.id = 'star:' + i;
+            element.addEventListener('click',(e)=>
+            {
+                console.log(e.target.id)
+                if(e.target.id === 'star:0')
+                {
+                    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+                        
+                        e.target.parentElement.children[i].src= 'assets/Images/StarEmpty.png';
+                    }
+                    e.target.parentElement.children[0].src ='assets/Images/StarFill.png';
+                }
+                if(e.target.id === 'star:1')
+                {
+                    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+                        
+                        e.target.parentElement.children[i].src= 'assets/Images/StarEmpty.png';
+                    }
+                    e.target.parentElement.children[0].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[1].src ='assets/Images/StarFill.png';
+                }
+                if(e.target.id === 'star:2')
+                {
+                    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+                        
+                        e.target.parentElement.children[i].src= 'assets/Images/StarEmpty.png';
+                    }
+                    e.target.parentElement.children[0].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[1].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[2].src ='assets/Images/StarFill.png';
+                }
+                if(e.target.id === 'star:3')
+                {
+                    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+                        
+                        e.target.parentElement.children[i].src= 'assets/Images/StarEmpty.png';
+                    }
+                    e.target.parentElement.children[0].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[1].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[2].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[3].src ='assets/Images/StarFill.png';
+                }
+                if(e.target.id === 'star:4')
+                {
+                    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+                        
+                        e.target.parentElement.children[i].src= 'assets/Images/StarEmpty.png';
+                    }
+                    e.target.parentElement.children[0].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[1].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[2].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[3].src ='assets/Images/StarFill.png';
+                    e.target.parentElement.children[4].src ='assets/Images/StarFill.png';
+                }
+            })
             element.src = 'assets/Images/StarFill.png'
+            
             if(i===4)
                 element.src = 'assets/Images/StarEmpty.png';
             starsContainer.append(element);
